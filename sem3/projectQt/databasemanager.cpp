dffd7865522731dcbf8c747b83dc17b383c7274a
NazTes <naztes2005@gmail.com>
2024-11-23 17:51:45 +0200

    Instead of using MySql starting to rewriten code to connect FireBase

--- sem3/projectQt/databasemanager.cpp
+++ sem3/projectQt/databasemanager.cpp
@@ -1,195 +1,45 @@
 #include "databasemanager.h"
 
-DatabaseManager::DatabaseManager() {
-    db=QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName("localhost");
-    db.setDatabaseName("finance_manager");
-    db.setUserName("root");
-    db.setPassword("qawsujikcvb2850147!");
+
+DatabaseManager::DatabaseManager(QObject*parent):QObject(parent)
+{
+    manager= new QNetworkAccessManager(this);
+    projectURL="";
 }
 
-
-bool DatabaseManager::connectToDatabase(){
-    if(!db.open()){
-        qDebug()<<"Database error:"<<db.lastError().text();
-        return false;
-    }
-    return true;
+QString DatabaseManager::constructUrl(const QString &path){
+    return projectURL+path+".json";
 }
 
-bool DatabaseManager::initializeDatabase(){
-    if(!connectToDatabase()){
-        return false;
+QJsonDocument DatabaseManager::synchronousRequest(const QString &path, const QString &method, const QJsonDocument &data){
+    QNetworkRequest request(constructUrl(path));
+    request.setHeader(QNetworkRequest::ContentTypeHeader,"application/json");
+
+    QNetworkReply*reply=nullptr;
+    if(method=="GET") {
+        reply=manager->get(request);
     }
-    return createTables();
+    else if(method=="POST"){
+        reply=manager->post(request,data.toJson());
+    }
+    else if(method=="PUT"){
+        reply=manager->put(request,data.toJson());
+    }
+    else if(method=="DELETE"){
+        reply=manager->deleteResource(request);
+    }
+    else if(method=="PATCH"){
+        reply=manager->sendCustomRequest(request,"PATCH",data.toJson());
+    }
+
+    QEventLoop loop;
+    connect(reply,&QNetworkReply::finished,&loop,&QEventLoop::quit);
+    loop.exec();
+
+    if(reply->error()==QNetworkReply::NoError){
+        QByteArray responseData=reply->readAll();
+        reply->deleteLater();
+        return QJsonDocument::fromJson(responseData);
+
+    }
 }
-
-bool DatabaseManager::createTables(){
-    QSqlQuery query;
-
-    QString createUsersTable=
-    "CREATE TABLE IF NOT EXISTS users("
-    "id INT AUTO_INCREMENT PRIMARY KEY,"
-    "login VARCHAR(50) UNIQUE NOT NULL,"
-    "email VARCHAR(100) UNIQUE NOT NULL,"
-    "PASSWORD VARCHAR(64) NOT NULL,"
-    "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
-                               ")ENGINE=InnoDB";
-    if(!query.exec(createUsersTable)){
-        qDebug()<<"Error creating users table:"<<query.lastError().text();
-        return false;
-    }
-    QString createTransactionsTable=
-    "CREATE TABLE IF NOT EXISTS transactions("
-    "id INT AUTO INCREMENT PRIMARY KEY,"
-    "user_id INT NOT NULL,"
-    "is_expense BOOLEAN NOT NULL,"
-    "category VARCHAR(50) NOT NULL,"
-    "amount DECIMAL(10,2) NOT NULL,"
-    "transaction_date DATETIME NOT NULL,"
-    "comment TEXT,"
-    "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,"
-    "FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE"
-                                      ")ENGINE=InnoDB";
-
-    if(!query.exec(createTransactionsTable)){
-        qDebug()<<"Error creating transactions table:"<<query.lastError().text();
-        return false;
-    }
-    return true;
-}
-
-//hash pass
-
-// QString DatabaseManager::hashPassword(const QString&password){
-//     QByteArray passwordBytes=password.toUft8();
-//     QByteArray hashedPassword= QCryptographicHash::hash(passwordBytes,QCryptographicHash::Sha256);
-//     return QString(hashedPassword.toHex());
-// }
-
-
-bool DatabaseManager::addUser(const QString&login, const QString&email, const QString&password){
-    if(userExists(login)||emailExists(email)){
-        return false;
-    }
-    QSqlQuery query;
-    query.prepare("INSERT INTO users(login, email,password)"
-                  "VALUES(:login,:email,:password)");
-    query.bindValue(":login",login);
-    query.bindValue(":email",email);
-    query.bindValue(":password",password);// additionaly can use hashing of pass
-    if(!query.exec()){
-        qDebug()<<"Error adding user:"<<query.lastError().text();
-        return false;
-    }
-    return true;
-}
-
-int DatabaseManager::validateUser(const QString&email, const QString&password){
-    QSqlQuery query;
-    query.prepare("SELECT id,password FROM users WHERE email=:email");
-    if(!query.exec()||!query.next()){
-        qDebug()<<"Not found users data";
-        return -1;
-    }
-    int userId=query.value(0).toInt();
-    QString storedPassword=query.value(1).toString();
-    if(storedPassword==password){
-        return userId;
-    }
-    return -1;
-}
-
-bool DatabaseManager::userExists(const QString& login){
-    QSqlQuery query;
-    query.prepare("SELECT COUNT(*) FROM users WHERE login=:login ");
-    query.bindValue(":login",login);
-    if(!query.exec()||!query.next()){
-        return false;
-    }
-    return query.value(0).toInt()>0;
-}
-
-bool DatabaseManager::emailExists(const QString&email){
-    QSqlQuery query;
-    query.prepare("SELECT COUNT(*) FROM users WHERE email=:email");
-    query.bindValue(":email",email);
-    if(!query.exec()||!query.next()){
-        return false;
-    }
-    return query.value(0).toInt()>0;
-}
-
-bool DatabaseManager::addTransaction(int userId, bool isExpense,const QString&category,double amount,
-                                    const QDateTime& date,const QString&comment){
-    QSqlQuery query;
-    query.prepare("INSERT INTO transactions (user_id,is_expense,category)"
-                  "amount, transaction_date,comment)"
-                  "VALUES(:user_id, :is_expense, :category, :amount,"
-                  ":transaction_date, :comment");
-    query.bindValue(":user_id",userId);
-    query.bindValue(":is_expense",isExpense);
-    query.bindValue(":category",category);
-    query.bindValue(":amount",amount);
-    query.bindValue(":transaction_date",date);
-    query.bindValue(":comment",comment);
-    if(!query.exec()){
-        qDebug()<<"Error adding transaction:"<<query.lastError().text();
-        return false;
-    }
-    return true;
-}
-bool DatabaseManager::updateTransaction(int transactionId,bool isExpense, const QString&category,
-                                        double amount, const QDateTime&date,const QString&comment){
-    QSqlQuery query;
-    query.prepare("UPDATE transactions SET is_expense=:is_expense,"
-                  "category=:category, amount=:amount, transaction_date=:transaction_date,"
-                  "comment=:comment WHERE id=:id");
-    query.bindValue(":id",transactionId);
-    query.bindValue(":is_expense",isExpense);
-    query.bindValue(":category",category);
-    query.bindValue(":amount",amount);
-    query.bindValue(":transaction_date",date);
-    query.bindValue(":comment",comment);
-
-    if(!query.exec()){
-        qDebug()<<"Error updating transaction:"<<query.lastError().text();
-        return false;
-    }
-    return true;
-}
-
-bool DatabaseManager::deleteTransaction(int transactionId){
-    QSqlQuery query;
-    query.prepare("DELETE FROM transactions WHERE id=:id");
-    query.bindValue(":id",transactionId);
-    if(!query.exec()){
-        qDebug()<<"Error deleting transaction:"<<query.lastError().text();
-        return false;
-    }
-    return true;
-}
-
-QSqlQuery DatabaseManager::getUserTransactions(int userId){
-    QSqlQuery query;
-    query.prepare("SELECT * FROM transactions WHERE user_id=:user_id"
-                  "ORDER BY transaction_date DESC");
-    query.bindValue(":user_id", userId);
-    if(!query.exec()){
-        qDebug()<<"Error getting user transactions:"<<query.lastError().text();
-    }
-    return query;
-}
-
-
-QSqlQuery DatabaseManager::getTransactionById(int transactionId){
-    QSqlQuery query;
-    query.prepare("SELECT*FROM transactions WHERE id:=id");
-    query.bindValue(":id",transactionId);
-    if(!query.exec()){
-        qDebug()<<"Error getting transaction data:"<<query.lastError().text();
-    }
-    return query;
-}
-
-
--- sem3/projectQt/databasemanager.h
+++ sem3/projectQt/databasemanager.h
@@ -1,38 +1,30 @@
 #ifndef DATABASEMANAGER_H
 #define DATABASEMANAGER_H
 
-#include <QtSql/QSqlDatabase> //basic class to open with Qt
-#include <QtSql/QSqlQuery> //class for querries
-#include <QtSql/QSqlError> //get text of the SQL error
-#include <QDebug>
-#include <QDateTime> //exact time
-#include <QCryptographicHash> //for hashing data
+#include<QtNetwork/QNetworkAccessManager> //HTTP/HTTPS requests
+#include<QtNetwork/QNetworkReply> //get reply form HTTP requests
+#include<QJsonDocument> //reading and modification of JSON docs
+#include<QJsonObject> //JSON obj (key-value)
+#include<QJsonArray>//JSON arrays
+#include<QDateTime>// time
+#include<QCryptographicHash> //password hashing
+#include<QEventLoop>//synchronization of asynchronous requests
+
 
 class DatabaseManager
 {
 public:
-    DatabaseManager();
+    explicit DatabaseManager(QObject*parent=nullptr);
 
-    bool initializeDatabase();
-    bool addUser(const QString&login, const QString&email,const QString&password);
-    int validateUser(const QString&email, const QString&password);
-    bool userExists(const QString& login);
-    bool emailExists(const QString &email);
+    bool intializeDatabase();
 
-    //methods to work with transactions
-    bool addTransaction(int userId,bool isExpense, const QString&category,
-                    double amount, const QDateTime&date,const QString&comment);
-    bool updateTransaction(int transactionId,bool isExpense, const QString&category,
-                           double amount,const QDateTime&date, const QString& comment);
-    bool deleteTransaction(int transactionId);
-    QSqlQuery getUserTransactions(int userId);
-    QSqlQuery getTransactionById(int transactionId);
 
 private:
-    QSqlDatabase db;
-    bool createTables();
-    QString hashPassword (const QString&password);
-    bool connectToDatabase();
+    QNetworkAccessManager*manager;
+    QString projectURL;
+    QString constructUrl(const QString&path);
+    QJsonDocument synchronousRequest(const QString&path, const QString&method,
+                                     const QJsonDocument&data=QJsonDocument());
 };
 
 #endif // DATABASEMANAGER_H
