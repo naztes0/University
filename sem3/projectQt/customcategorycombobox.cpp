3da98b379d8a5952562f1a56665c328b68326dd0
NazTes <naztes2005@gmail.com>
2024-12-01 15:38:06 +0200

    Created custom categories combobox in transaction adding window. To combobox was added additional button to add own categories, and implemanted the deleting of categories. When user points by cursor in cateogry, appears a button against the name of category. By clicking it appears a window of assuarence (do user really wanna to delete it)

--- sem3/projectQt/customcategorycombobox.cpp
+++ sem3/projectQt/customcategorycombobox.cpp
@@ -1,14 +1,80 @@
 #include "customcategorycombobox.h"
 #include "ui_customcategorycombobox.h"
 
+
+
+CategoryItemDelegate::CategoryItemDelegate(QObject *parent)
+    :QStyledItemDelegate(parent){}
+
+void CategoryItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const{
+    QStyledItemDelegate::paint(painter,option,index);
+
+    //if this is not the first item(placeholder)
+    if(index.row()>0&&option.state&QStyle::State_MouseOver){
+        QRect iconRect(option.rect.right()-20,option.rect.top()+2,12,12);
+        QIcon deleteIcon(":/img/img/trash.svg");
+        deleteIcon.paint(painter,iconRect);
+    }
+}
+bool CategoryItemDelegate::editorEvent(QEvent *event, QAbstractItemModel *model, const QStyleOptionViewItem &option, const QModelIndex &index){
+    if(index.row()==0){
+        return QStyledItemDelegate::editorEvent(event,model,option,index);
+    }
+    if (event->type() == QEvent::MouseButtonPress) {
+        QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
+        QRect iconRect(option.rect.right() - 20, option.rect.top() + 2, 16, 16);
+
+        // Check if click is on delete icon
+        if (iconRect.contains(mouseEvent->pos())) {
+            emit deleteCategory(index.data().toString());
+            return true;
+        }
+    }
+
+    return QStyledItemDelegate::editorEvent(event, model, option, index);
+}
+//Custom category ComboBox Implementation
 CustomCategoryComboBox::CustomCategoryComboBox(QWidget *parent)
-    : QWidget(parent)
+    : QComboBox(parent),manager(nullptr),m_userId(-1)
     , ui(new Ui::CustomCategoryComboBox)
 {
     ui->setupUi(this);
+    m_addButton=new QPushButton("+",this);
+    m_addButton->setFixedSize(20,20);
+    connect(m_addButton,&QPushButton::clicked,this,&CustomCategoryComboBox::onAddCategoryClicked);
+
+    // Set custom item delegate
+    CategoryItemDelegate *delegate = new CategoryItemDelegate(this);
+    this->setItemDelegate(delegate);
+
+    // Connect delegate's delete signal
+    connect(delegate, &CategoryItemDelegate::deleteCategory,
+            this, &CustomCategoryComboBox::deleteCategoryRequested);
+
 }
 
 CustomCategoryComboBox::~CustomCategoryComboBox()
 {
     delete ui;
+}
+
+void CustomCategoryComboBox::paintEvent(QPaintEvent *event) {
+    QComboBox::paintEvent(event);
+
+    // Position add button at the right side of the combobox
+    m_addButton->move(width() - m_addButton->width() - 2,
+                      (height() - m_addButton->height()) / 2);
+}
+
+void CustomCategoryComboBox::mousePressEvent(QMouseEvent *event) {
+    // Check if click is outside the main combo box area (on the add button)
+    if (event->pos().x() >= width() - m_addButton->width()) {
+        m_addButton->click();
+        return;
+    }
+    QComboBox::mousePressEvent(event);
+}
+
+void CustomCategoryComboBox::onAddCategoryClicked() {
+    emit addCategoryRequested();
 }
--- sem3/projectQt/customcategorycombobox.h
+++ sem3/projectQt/customcategorycombobox.h
@@ -7,22 +7,51 @@
 #include<QPushButton>
 #include<QHBoxLayout>
 #include<QLabel>
-#include<MouseEvent>
+#include<QMouseEvent>
+#include"databasemanager.h"
+#include<QPainter>
+#include<QInputDialog>
+#include<QMessageBox>
 
 namespace Ui {
 class CustomCategoryComboBox;
 }
 
-class CustomCategoryComboBox : public QWidget
+class CategoryItemDelegate:public QStyledItemDelegate{
+    Q_OBJECT
+public:
+    explicit CategoryItemDelegate(QObject*parent=nullptr);
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+signals:
+    void deleteCategory(const QString& category) const;
+protected:
+    bool editorEvent(QEvent *event, QAbstractItemModel *model, const QStyleOptionViewItem &option, const QModelIndex &index);
+};
+
+class CustomCategoryComboBox : public QComboBox
 {
     Q_OBJECT
 
 public:
     explicit CustomCategoryComboBox(QWidget *parent = nullptr);
     ~CustomCategoryComboBox();
+    void setDBManager(DatabaseManager* dbManager, int userId){manager = dbManager,m_userId = userId;}
+signals:
+    void addCategoryRequested();
+    void deleteCategoryRequested(const QString& category);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
 
 private:
     Ui::CustomCategoryComboBox *ui;
+    DatabaseManager* manager;
+    int m_userId;
+    QPushButton* m_addButton;
+private slots:
+    void onAddCategoryClicked();
+
 };
 
 #endif // CUSTOMCATEGORYCOMBOBOX_H
