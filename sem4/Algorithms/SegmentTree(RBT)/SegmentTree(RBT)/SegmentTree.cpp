ae4dab6656d58a38f2fc39c88e11c4063e0d4914
NazTes <naztes2005@gmail.com>
2025-03-04 23:42:39 +0200

    Added method update to update the sum value in some ranges , method remove and additional to it that removes nodes

--- sem4/Algorithms/SegmentTree(RBT)/SegmentTree(RBT)/SegmentTree.cpp
+++ sem4/Algorithms/SegmentTree(RBT)/SegmentTree(RBT)/SegmentTree.cpp
@@ -99,8 +99,160 @@ int SegmentTree::queryPrivate(Node* node, int l, int r) {
 
 	if (l <= node->left && node->right <= r)	return node->sum;
 
-	return query(node->leftChild, l, r) + query(node->rightChild, l, r);
+	return queryPrivate(node->leftChild, l, r) + queryPrivate(node->rightChild, l, r);
 }
+
+void SegmentTree::updatePrivate(Node* node, int l, int r, int newValue) {
+	if (node == NIL || l > node->right || r < node->left) {
+		return; // Відрізок взагалі не перетинається
+	}
+
+	if (l <= node->left && node->right <= r) {
+		node->sum += (node->right - node->left + 1) * newValue; // Оновлюємо суму
+	}
+	else {
+		// Часткове перекриття - оновлюємо дітей
+		updatePrivate(node->leftChild, l, r, newValue);
+		updatePrivate(node->rightChild, l, r, newValue);
+
+		// Після оновлення дітей оновлюємо поточну суму
+		node->sum = node->leftChild->sum + node->rightChild->sum;
+	}
+}
+
+//Methods for removing nodes
+void SegmentTree::transplant(Node* u, Node* v) {
+	if (u->parent == NIL) {
+		root = v;
+	}
+	else if (u == u->parent->leftChild) {
+		u->parent->leftChild = v;
+	}
+	else {
+		u->parent->rightChild = v;
+	}
+	v->parent = u->parent;
+}
+
+SegmentTree::Node* SegmentTree::minimum(Node* node) {
+	while (node->leftChild != NIL) {
+		node = node->leftChild;
+	}
+	return node;
+}
+void SegmentTree::fixDelete(Node* node) {
+	while (node != root && node->color == true) {
+		if (node == node->parent->leftChild) {
+			Node* sibling = node->parent->rightChild;
+
+			if (sibling->color == false) {
+				sibling->color = true;
+				node->parent->color = false;
+				leftRotate(node->parent);
+				sibling = node->parent->rightChild;
+			}
+
+			if (sibling->leftChild->color == true && sibling->rightChild->color == true) {
+				sibling->color = false;
+				node = node->parent;
+			}
+			else {
+				if (sibling->rightChild->color == true) {
+					sibling->leftChild->color = true;
+					sibling->color = false;
+					rightRotate(sibling);
+					sibling = node->parent->rightChild;
+				}
+
+				sibling->color = node->parent->color;
+				node->parent->color = true;
+				sibling->rightChild->color = true;
+				leftRotate(node->parent);
+				node = root;
+			}
+		}
+		else {
+			Node* sibling = node->parent->leftChild;
+
+			if (sibling->color == false) {
+				sibling->color = true;
+				node->parent->color = false;
+				rightRotate(node->parent);
+				sibling = node->parent->leftChild;
+			}
+
+			if (sibling->leftChild->color == true && sibling->rightChild->color == true) {
+				sibling->color = false;
+				node = node->parent;
+			}
+			else {
+				if (sibling->leftChild->color == true) {
+					sibling->rightChild->color = true;
+					sibling->color = false;
+					leftRotate(sibling);
+					sibling = node->parent->leftChild;
+				}
+
+				sibling->color = node->parent->color;
+				node->parent->color = true;
+				sibling->leftChild->color = true;
+				rightRotate(node->parent);
+				node = root;
+			}
+		}
+	}
+	node->color = true;
+}
+void SegmentTree::removeNode(Node* node, int l, int r) {
+	if (node == NIL) return;
+
+	if (l < node->left) {
+		removeNode(node->leftChild, l, r);
+	}
+	else if (l > node->left) {
+		removeNode(node->rightChild, l, r);
+	}
+	else {
+		Node* y = node;
+		Node* x;
+		bool yOriginalColor = y->color;
+
+		if (node->leftChild == NIL) {
+			x = node->rightChild;
+			transplant(node, node->rightChild);
+		}
+		else if (node->rightChild == NIL) {
+			x = node->leftChild;
+			transplant(node, node->leftChild);
+		}
+		else {
+			y = minimum(node->rightChild);
+			yOriginalColor = y->color;
+			x = y->rightChild;
+
+			if (y->parent == node) {
+				x->parent = y;
+			}
+			else {
+				transplant(y, y->rightChild);
+				y->rightChild = node->rightChild;
+				y->rightChild->parent = y;
+			}
+
+			transplant(node, y);
+			y->leftChild = node->leftChild;
+			y->leftChild->parent = y;
+			y->color = node->color;
+		}
+
+		delete node;
+
+		if (yOriginalColor == true) { // Якщо видалявся чорний вузол
+			fixDelete(x);
+		}
+	}
+}
+
 
 ////////////PUBLIC//////////////////////
 void SegmentTree::insert(int l, int r, int sum) {
@@ -140,3 +292,10 @@ int SegmentTree::query(int l, int r) {
 	queryPrivate(root, l, r);
 }
 
+void SegmentTree::update(int l, int r, int newValue) {
+	updatePrivate(root, l, r, newValue);
+}
+
+void SegmentTree::remove(int l, int r) {
+	removeNode(root, l, r);
+}
--- sem4/Algorithms/SegmentTree(RBT)/SegmentTree(RBT)/SegmentTree.h
+++ sem4/Algorithms/SegmentTree(RBT)/SegmentTree(RBT)/SegmentTree.h
@@ -19,24 +19,30 @@ private:
 	};
 
 	Node* root;
-	Node* NIL;
+	Node* NIL; //equ to the nullptr in RBT
 
 	
-	void rightRotate(Node* x);
+	void rightRotate(Node* x); 
 	void leftRotate(Node* x);
-	void fixInsert(Node* node);
-	int queryPrivate(Node* node, int l, int r);
-		
+	void fixInsert(Node* node); //fixing the tree due to RBT rules after the insertion
+	int queryPrivate(Node* node, int l, int r); //Get the sum number on some range
+	void updatePrivate(Node* node, int l, int r, int newValue);
+	
+	//Remove methods
+	void removeNode(Node* node, int l, int r);
+	void transplant(Node* u, Node* v);
+	Node* minimum(Node* node); 
+	void fixDelete(Node* node);
 
 public:
-	SegmentTree() {
+	SegmentTree() { 
 		NIL = new Node(0,0,0);
 		NIL->color = false;
 		root = NIL;
 	}
-	void insert(int l, int r, int sum);
-	int query(Node*node,int l, int r);
-	int query(int l, int r);
-
+	void insert(int l, int r, int sum); //insertion of node 
+	int query(int l, int r); //the call of private method querry 
+	void update(int l, int r, int newValue); //change the sum on some range and update sum in other nodes
+	void remove(int l, int r);
 };
 
