959291a200194dfe2c273bc8f7b7c6d3cb24b1e4
NazTes <naztes2005@gmail.com>
2025-03-15 22:26:36 +0200

    Was added Clerk so now the authenitcation works

--- sem4/OOP/lab1/client/src/components/Navbar.jsx
+++ sem4/OOP/lab1/client/src/components/Navbar.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
+  const { openSignIn } = useClerk();
+  const { isSignedIn, user } = useUser();
   return (
-    <div className='flex items-center justify-between mx-4 py-3 lg:mx-44'>
-     <Link to='/'> <img className='w-32 sm:w-44' src={assets.logo}alt="" /></Link>
-      <button className='bg-zinc-800 text-white flex items-center gap-5 px-4 py-1.5 sm:px-8 sm:py-3 sm:gap-8 text-sm rounded-full'>Get started <img className="w-3 sm:w-4"src={assets.arrow_icon} alt="" /></button>
+    <div className="flex items-center justify-between mx-4 py-3 lg:mx-44">
+      <Link to="/">
+        {" "}
+        <img className="w-32 sm:w-44" src={assets.logo} alt="" />
+      </Link>
+      {isSignedIn ? (
+        <div>
+          <UserButton />
+        </div>
+      ) : (
+        <button
+          onClick={() => openSignIn({})}
+          className="bg-zinc-800 text-white flex items-center gap-5 px-4 py-1.5 sm:px-8 sm:py-3 sm:gap-8 text-sm rounded-full"
+        >
+          Get started{" "}
+          <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
