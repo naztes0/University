13df9975ac810d18596b52751f608ba0d749755d
NazTes <naztes2005@gmail.com>
2025-03-16 00:54:52 +0200

    Connected mongoDB. Created userSchema Edited mongo in .env. Added gitignore

--- sem4/OOP/lab1/server/server.js
+++ sem4/OOP/lab1/server/server.js
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import connectDB from './configs/mongodb.js'
 
 //App config
 
 const PORT=process.env.PORT||4000;
 const app =express();
+await connectDB();
 
 //Initialize Middlewares
 app.use(express.json())
@@ -14,3 +16,4 @@ app.use(cors())
 //API routes
 app.get('/',(req,res)=>res.send("API Working"))
 app.listen(PORT,()=>console.log("Server Running on port "+PORT))
+
