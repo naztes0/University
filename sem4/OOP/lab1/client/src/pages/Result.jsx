d8532c1f49903b5f118b50c4cbb8a2f91e0e22d1
NazTes <naztes2005@gmail.com>
2025-03-15 17:47:25 +0200

    Added design for result page

--- sem4/OOP/lab1/client/src/pages/Result.jsx
+++ sem4/OOP/lab1/client/src/pages/Result.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Result = () => {
   return (
-    <div>Result</div>
+    <div className='mx-4 my-2 lg:mx-44 mt-9 mb-14 min-h-[80vh] sm:min-h-[75vh]'>
+      <div className='bg-white rounded-lg px-8 py-10 pb-12 drop-shadow-md'>
+          {/* Image Container */}
+          <div className='flex flex-col sm:grid grid-cols-2 gap-8 '>
+              {/* Left Side */}
+              <div >
+                <p className='font-semibold text-gray-600 mb-2'>Original</p>
+                <img className='rounded-md border' src={assets.image_w_bg} alt="" />
+              </div>
+              {/* Right Side */}
+              <div className='flex flex-col'>
+                  <p className='font-semibold text-gray-600 mb-2'>Background Removed</p>
+                  <div className='rounded-md border border-gray-300 h-full relative bg-layer overflow-hidden'>
+                    <img src={assets.image_wo_bg} alt="" />
+                    {/* <div className='absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2'>
+                      <div className='border-4 border-violet-600 rounded-full h-12 w-12 border-t-transparent animate-spin'>
+                      </div>
+                    </div> */}
+                  </div>  
+
+              </div>
+          </div>
+
+           {/* BUTTONS */}
+            <div className='flex justify-center sm:justify-end items-center flex-wrap mt-8 gap-4'>
+              <button className='px-8 py-2.5 text-violet-600 text-sm border border-violet-600 rounded-full hover:scale-105 transition-all duration-500'>Try another image</button>
+              <a className='px-8 py-2.5 bg-gradient-to-tr from-violet-700 to-fuchsia-500 text-white rounded-full hover:scale-105 transition-all duration-500'href="">Download image</a>
+            </div>
+      </div>
+    </div>
   )
 }
 
