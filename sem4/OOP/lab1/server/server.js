2abc3c5968f85876f4036deea835da6514130604
NazTes <naztes2005@gmail.com>
2025-03-16 18:51:59 +0200

    Mounting userRouter on a path

--- sem4/OOP/lab1/server/server.js
+++ sem4/OOP/lab1/server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js';
 
 //App config
 
@@ -15,6 +16,8 @@ app.use(cors())
 
 //API routes
 app.get('/',(req,res)=>res.send("API Working"))
+app.use('/api/user',userRouter)
+
 app.listen(PORT,()=>console.log("Server Running on port "+PORT))
 
 connectDB()
