45618edc5727e46bd18925f73e12b3e8dfb3c38a
NazTes <naztes2005@gmail.com>
2025-03-16 12:44:00 +0200

    try1

--- sem4/OOP/lab1/server/configs/mongodb.js
+++ sem4/OOP/lab1/server/configs/mongodb.js
@@ -1,29 +1,11 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-    const isProduction = process.env.NODE_ENV === 'production';
-    
-    // Більш жорсткі таймаути для Vercel
-    const options = {
-        serverSelectionTimeoutMS: isProduction ? 3000 : 5000,
-        socketTimeoutMS: isProduction ? 5000 : 30000,
-        connectTimeoutMS: isProduction ? 5000 : 30000,
-        ssl: true,
-        retryWrites: true,
-        w: "majority"
-    };
-    
-    try {
-        await mongoose.connect(process.env.MONGO_URI, options);
-        return true;
-    } catch (error) {
-        console.error("Failed to connect to MongoDB:", error);
-        // В production не завершуємо процес при помилці підключення
-        if (!isProduction) {
-            process.exit(1);
-        }
-        return false;
-    }
-};
+const connectDB= async()=>{
 
-export default connectDB;
+    mongoose.connection.on("connected",()=>{
+        console.log("Database connected");
+    })
+    await mongoose.connect(`${process.env.MONGO_URI}`);
+}
+
+export default connectDB
