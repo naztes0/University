393a7fdbe0e33172a7679da55f6e86c0f28ef31c
NazTes <naztes2005@gmail.com>
2025-03-16 13:01:00 +0200

    try4

--- sem4/OOP/lab1/server/configs/mongodb.js
+++ sem4/OOP/lab1/server/configs/mongodb.js
@@ -5,7 +5,7 @@ const connectDB= async()=>{
     mongoose.connection.on("connected",()=>{
         console.log("Database connected");
     })
-    await mongoose.connect(`${process.env.MONGODB_URI}/University`);
+    await mongoose.connect(process.env.MONGODB_URI);
 }
 
 export default connectDB
