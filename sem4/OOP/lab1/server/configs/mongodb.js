7d2a126c819ddc3a796f1ab7ea49a61ac72cb2ab
NazTes <naztes2005@gmail.com>
2025-03-16 13:06:55 +0200

    Edites to make the vercel operate

--- sem4/OOP/lab1/server/configs/mongodb.js
+++ sem4/OOP/lab1/server/configs/mongodb.js
@@ -1,11 +1,21 @@
 import mongoose from "mongoose";
 
-const connectDB= async()=>{
-
-    mongoose.connection.on("connected",()=>{
-        console.log("Database connected");
-    })
-    await mongoose.connect(process.env.MONGODB_URI);
+const connectDB = async () => {
+    try {
+        mongoose.connection.on("connected", () => {
+            console.log("Database connected");
+        });
+        
+        const options = {
+            serverSelectionTimeoutMS: 5000,
+            connectTimeoutMS: 5000
+        };
+        
+        await mongoose.connect(`${process.env.MONGO_URI}`, options);
+        return true;
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        return false; 
+    }
 }
-
 export default connectDB
--- sem4/OOP/lab1/server/server.js
+++ sem4/OOP/lab1/server/server.js
@@ -7,7 +7,7 @@ import connectDB from './configs/mongodb.js'
 
 const PORT=process.env.PORT||4000;
 const app =express();
-await connectDB();
+
 
 //Initialize Middlewares
 app.use(express.json())
@@ -17,3 +17,9 @@ app.use(cors())
 app.get('/',(req,res)=>res.send("API Working"))
 app.listen(PORT,()=>console.log("Server Running on port "+PORT))
 
+connectDB()
+  .then(() => {
+  })
+  .catch(err => {
+    console.error("DB Connection Error:", err);
+  });
