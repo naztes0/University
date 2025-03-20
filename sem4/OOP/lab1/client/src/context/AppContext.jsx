caac1b92dd03985d64210c63894e5abbe2fbc05d
NazTes <naztes2005@gmail.com>
2025-03-20 19:08:43 +0200

    written App context to share value{} values with <App/>

---
+++ sem4/OOP/lab1/client/src/context/AppContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useState } from "react";
+import { useAuth } from "@clerk/clerk-react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export const AppContext = createContext();
+
+const AppContextProvider = (props) => {
+  const [credit, setCredit] = useState(false);
+
+  const backenUrl = import.meta.env.VITE_BACKEND_URL;
+  const { getToken } = useAuth();
+
+  const loadCreditsData = async () => {
+    try {
+      const token = await getToken();
+      const { data } = await axios.get(backenUrl + "/api/user/credits", {
+        headers: { token },
+      });
+      if (data.success) {
+        setCredit(data.credits);
+        console.log(data.credits);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+  const value = {
+    credit,
+    setCredit,
+    loadCreditsData,
+    backenUrl,
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
