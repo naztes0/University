b8974b7c33f4a9e1f3096ee69ecb1933e8b787ce
NazTes <naztes2005@gmail.com>
2025-03-30 23:01:40 +0300

    corrections

--- sem4/OOP/lab1/client/src/App.jsx
+++ sem4/OOP/lab1/client/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import Result from "./pages/Result";
 import BuyCredit from "./pages/BuyCredit";
 import Navbar from "./components/Navbar";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import { ToastContainer, toast } from "react-toastify";
 import CreditTimer from "./components/CreditTimer";
 
