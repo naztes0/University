13d9fde947b6bd9c060b41d2a6bbd5a7aaede3b1
NazTes <naztes2005@gmail.com>
2025-03-30 22:15:24 +0300

    Corrected the responsive layout to make the elements correctly placed for small devices

--- sem4/OOP/lab1/client/src/components/Navbar.jsx
+++ sem4/OOP/lab1/client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { AppContext } from "../context/AppContext";
 const Navbar = () => {
   const { openSignIn } = useClerk();
   const { isSignedIn, user } = useUser();
-  const { credit, loadCreditsData } = useContext(AppContext);
+  const { credit, loadCreditsData, timerActive } = useContext(AppContext);
 
   useEffect(() => {
     if (isSignedIn) {
@@ -16,20 +16,20 @@ const Navbar = () => {
   }, [isSignedIn]);
 
   return (
-    <div className="flex items-center justify-between mx-4 py-3 lg:mx-44">
+    <div className="flex items-center justify-between mx-5 py-3 lg:mx-44">
       <Link to="/">
         {" "}
         <img className="w-32 sm:w-44" src={assets.logo} alt="" />
       </Link>
       {isSignedIn ? (
         <div className="flex items-center gap-2 sm:gap-3">
-          <button className="flex items-center gap-2 bg-blue-100 px-4 sm:px-7 sm:py-2.5 rounded-full hover:scale-105 transition-all duration-700">
+          <button className="flex items-center gap-2 bg-blue-100 px-4 sm:px-5 py-2 md:py-3 rounded-full hover:scale-105 transition-all duration-700">
             <img className="w-5" src={assets.credit_icon} alt="" />
             <p className="text-xs sm:text-sm font-medium text-gray-600">
               Credits: {credit}
             </p>
           </button>
-          <p className="text-gray-600">Hi,{user.fullName}</p>
+          <p className="text-gray-600 hidden lg:block ">Hi, {user.fullName}</p>
           <UserButton />
         </div>
       ) : (
