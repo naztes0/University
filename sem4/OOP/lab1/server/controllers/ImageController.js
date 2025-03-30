7e71001a7802fa99d9a56e5bb22a02758b914670
NazTes <naztes2005@gmail.com>
2025-03-30 22:36:48 +0300

    Was changed the method of minusing credits. Used the values for front

--- sem4/OOP/lab1/server/controllers/ImageController.js
+++ sem4/OOP/lab1/server/controllers/ImageController.js
@@ -36,13 +36,42 @@ const removeBgImage=async(req,res)=>{
 
         const resultImage = `data: ${req.file.mimetype};base64,${base64Image}`
 
-        //To minus one credit for removing img bg
+        // //To minus one credit for removing img bg
 
         // await userModel.findByIdAndUpdate(user._id,{creditBalance:user.creditBalance-1})
 
      
-        res.json({success:true, resultImage, creditBalance:user.creditBalance-1, message:"Background Removed"})
+        // res.json({success:true, resultImage, creditBalance:user.creditBalance-1, message:"Background Removed"})
 
+        // To minus one credit for removing img bg
+        const newCreditBalance = user.creditBalance - 1;
+        
+        // Check if we need to activate the timer
+        let timerActive = user.timerActive;
+        let nextCreditAt = user.nextCreditAt;
+        
+        // If credits are below max and timer isn't active, activate it
+        if (newCreditBalance < 5 && !timerActive) {
+            timerActive = true;
+            nextCreditAt = new Date(new Date().getTime() +  3*60 * 1000); //3 * 60 *
+        }
+        
+        // Update user with new credit balance and timer status
+        await userModel.findByIdAndUpdate(user._id, {
+            creditBalance: newCreditBalance,
+            timerActive,
+            nextCreditAt,
+            lastCreditUpdate: new Date()
+        });
+
+        res.json({
+            success: true,
+            resultImage,
+            creditBalance: newCreditBalance,
+            timerActive,
+            nextCreditAt,
+            message: "Background Removed"
+        })
 
     } catch (error) {
         console.log(error.message)
