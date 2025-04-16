85156021d5c0948fcecac605171c50c26db44fb8
naztes0 <naztes0@gmail.com>
2025-04-16 12:16:43 +0300

    Corrected a mistake

--- sem4/OOP/lab1/original/server/middlewares/multer.js
+++ sem4/OOP/lab1/original/server/middlewares/multer.js
@@ -2,12 +2,12 @@ import multer from "multer";
 
 //creating multer middleware fro parsing formdata
 
-const storage= multer.diskStorage({
-    filename: function(re,file,callback){
-        callback(null,`${Date.now()}_${file.originalname}`)
+const storage = multer.diskStorage({
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
     }
 })
 
-const upload = multer({storage})
+const upload = multer({ storage })
 
 export default upload 
