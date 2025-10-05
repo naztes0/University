c739ef2141ff00a606808146f5aaf1b07f4b3373
naztes0 <naztes0@gmail.com>
2025-10-05 19:39:25 +0300

    Seoarated consts to individual file

---
+++ sem5/OOP/lab1/src/main/java/com/lab1/util/AppConstants.java
@@ -0,0 +1,10 @@
+package com.lab1.util;
+
+public final class AppConstants {
+    private AppConstants() {
+    }
+
+    public static final String COMPILATION_FOLDER = "compilations_folder";
+    public static final String FILE_EXTENSION = ".txt";
+
+}
