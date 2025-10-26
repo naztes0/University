fd8c3d7f9329e3689fb9655a152fd11d69176365
naztes0 <naztes0@gmail.com>
2025-10-26 15:07:14 +0200

    Added check of function correctnes execution (results satisfie, stores in results dir)

--- sem5/ComplexSystems/lab2/main.py
+++ sem5/ComplexSystems/lab2/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import pim_methods as pim
 import os 
 
 
@@ -7,8 +8,11 @@ import os
 def read_img():
     x3_img=cv2.imread("x3.bmp", cv2.IMREAD_GRAYSCALE)
     y3_img=cv2.imread("y3.bmp", cv2.IMREAD_GRAYSCALE)
+    # print("x3:", type(x3_img), x3_img is None)
+    # print(x3_img.shape)
+    # print(x3_img.dtype)
+
     #show imgs
-     #display the image
     cv2.imshow("Image X3", x3_img)
     cv2.imshow("Image Y3", y3_img)
     #wait for the user to press a key
@@ -26,8 +30,8 @@ def read_img():
 def save_image(matrix, filename="output.bmp"):
     #make folder if not exists
     dir_res= "results"
-    if os.path.exists(dir_res):
-        os.mkdir(dir_res)
+    if not os.path.exists(dir_res):
+        os.makedirs(dir_res)
     
     if not filename.endswith(".bmp"):
         filename=filename + ".bmp"
@@ -47,12 +51,44 @@ def save_image(matrix, filename="output.bmp"):
     
     return img
 
+def find_operator(X,Y,pim_func,V=None,eps=1e-6, delta=1000):
+    if V is None:
+        V = np.zeros((Y.shape[0], X.shape[0]))
+    
+    X_pi=pim_func(X,eps,delta)
+
+    if not (pim.isPseudoInversed(X,X_pi)): return
+
+    #General formula: A=YX+ + VZ^T(X^T)
+    #Find Z(X^T)=I_m-XX+
+    Z=np.identity(X.shape[0])-X@X_pi
+    YX_pi= Y@X_pi
+    VZ=V@Z.T
+    A=YX_pi+VZ
+    return A
+    
+def applyOperator(X,A):
+    return A@X
 
 def main():
     #Firstly init and fill X, Y matrix from imgs
-    X,Y= read_img
+    X,Y= read_img()
+    # print("\nX mtrix:\n")
+    # for i in range (X.shape[0]):
+    #     for j in range(X.shape[1]):
+    #         print(X[i,j])
 
     save_image(Y,"Y_orig")
-    result={}
 
+    #Moore-Penrose method
+    A_MP=find_operator(X,Y,pim.pim_MoorePenrose, eps=1e-6)
+    Y_MP=applyOperator(X,A_MP)
+
+    A_MG=find_operator(X,Y, pim.pim_Grevilles, eps=1e-6)
+    save_image(Y_MP,"MP_result")
     
+if __name__ == "__main__":
+    main()
+
+
+
