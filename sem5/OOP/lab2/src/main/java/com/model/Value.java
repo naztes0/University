156beb71201aad95e477da0e5960d2da7b3ccbe4
naztes0 <naztes0@gmail.com>
2025-11-29 15:31:35 +0200

    addded class Value

---
+++ sem5/OOP/lab2/src/main/java/com/model/Value.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.util.Objects;
+
+/**
+ * Value information - whether knife is collectible
+ */
+public class Value {
+    private boolean collectible;
+
+    public Value() {
+    }
+
+    public Value(boolean collectible) {
+        this.collectible = collectible;
+    }
+
+    public boolean isCollectible() {
+        return collectible;
+    }
+
+    public void setCollectible(boolean collectible) {
+        this.collectible = collectible;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Value value = (Value) o;
+        return collectible == value.collectible;
+    }
+
+    @Override
+    public String toString() {
+        return "Value{" +
+                "collectible=" + collectible +
+                '}';
+    }
+}
